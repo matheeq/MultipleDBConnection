e5a587f40d07b858a966483f12fd404fdf583213
matheeq <121287882+matheeq@users.noreply.github.com>
2025-07-01 16:23:31 +0530

    Add files via upload
    propertyFiles

---
+++ PropertyFileOperationsDemo/src/main/java/com/FileOperations/PropertyFileOperationsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.FileOperations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyFileOperationsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyFileOperationsDemoApplication.class, args);
+	}
+
+}
